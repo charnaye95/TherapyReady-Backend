1070acfa783cf018239ab9baee13f2829d90a698
Charnaye Grier <charnayegrier@gmail.com>
2022-11-28 12:33:15 -0500

    attempt 2

--- TherapyReady_Backend/settings.py
+++ TherapyReady_Backend/settings.py
@@ -150,7 +150,3 @@ REST_FRAMEWORK = {
 }
 
 STATIC_ROOT=os.path.join(BASE_DIR, "static/")
-
-# STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
-# ]
